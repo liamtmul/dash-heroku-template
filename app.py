95971e3caabe7f8586a9f90c825351113258f08a
liamtmul <50001904+liamtmul@users.noreply.github.com>
2020-08-09 18:27:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -115,8 +115,9 @@ fig6.update_layout(showlegend=False)
 fig6.for_each_annotation(lambda a: a.update(text=a.text.replace("sex=", "")))
 
 #app = JupyterDash(__name__, external_stylesheets=external_stylesheets)
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets))
 
+server=app.server
 
 app.layout = html.Div(
     [
