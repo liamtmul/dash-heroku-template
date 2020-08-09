a7fa6b4a3aad044c026eb0c16901029592641843
liamtmul <50001904+liamtmul@users.noreply.github.com>
2020-08-09 16:57:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,4 @@ app.layout = html.Div(
 )
 
 if __name__ == '__main__':
-    #app.run_server( debug=True)
-    app.run_server(mode='inline', debug=True, port=591)
-if __name__ == '__main__':
-    app.run_server(debug=True, port=8051, host='0.0.0.0')
+    app.run_server( debug=True)
